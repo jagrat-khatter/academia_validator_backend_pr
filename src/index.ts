4ec66d9c3d11c82a98d6ff9de55614c26a64aa2f
jagrat-khatter <jagratkhatter@gmail.com>
2025-09-09 19:09:11 +0000

    For deploy ment

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import  type {Request  , Response , NextFunction } from 'express'
 import cors from 'cors'
 import {class12_router} from './routes/cbse_class12'
-
+const PORT = process.env.PORT || 3000;
 
 
 const app = express();
@@ -11,6 +11,6 @@ app.use(express.json());
 
 app.use('/cbseclass12' , class12_router);
 
-app.listen(3000 , ()=>{
-    console.log("Listening on port 3000")
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
 })
